08bbf79ad5d272b7f630fd27c684566b698f4201
Zahidul Hossain <choyan.009@gmail.com>
2014-07-03 22:23:25 +0600

    Update ls.js
    Trying to implement Keyboard event

--- js/ls.js
+++ js/ls.js
@@ -9,21 +9,21 @@
         var iw = 800; //image width
         var ns = cw - iw; //negative space
         var lm = ns / 2; //left margin
-        var mr=100; //margin-right of each li
-
+        var mr = 100; //margin-right of each li
+        var img = "#justslider li>img";
 
         $("#justslider ul.ls").css({
             marginLeft: lm
         });
 
 
-        $("#justslider li>img").each(function (i) {
+        $(img).each(function (i) {
             $(this).data("index", i);
         })
 
-        $("#justslider li>img").bind("click", function () {
+        var eventFunc = function (img) {
             var index = $(this).data("index");
-            var newlm = lm - index * (iw+mr); //new left margin value
+            var newlm = lm - index * (iw + mr); //new left margin value
 
             $("#justslider ul.ls").css({
                 marginLeft: newlm
@@ -36,6 +36,15 @@
             $(this).css({
                 opacity: "1"
             });
-        })
+
+        }
+        $(img).click(eventFunc);
+        $(document).keydown(function (e) {
+            if (e.keyCode == 37 || e.keyCode == 39) {
+                eventFunc();
+                e.preventDefault();
+            }
+        });
+
     });
 })(jQuery);
