9dcf09f474d9716ebc51b422186598148db85703
Anik Hasan <stackoverflow789@gmail.com>
2014-07-04 02:59:43 +0600

    replaced marginLeft with transform

--- js/justslide.jquery.js
+++ js/justslide.jquery.js
@@ -4,55 +4,61 @@
  * Release Date: 3rd July, 2014
  * Version 0.1
  */
-(function($){
+(function($) {
     "use strict";
     var container;
     var defaults = {
-        fullWidth:"off",
-        opacity:0.6,
-        imageWidth:800,
-        gap:100
+        fullWidth: "off",
+        opacity: 0.6,
+        imageWidth: 800,
+        gap: 100
     };
 
-    $.fn.justSlide = function(options){
+    $.fn.justSlide = function(options) {
 
         container = this;
         $(this).addClass("jscontainer");
 
-        if($(this).length<=0) return false;
-        var opts = $.extend(defaults,options);
+        if ($(this).length <= 0) return false;
+        var opts = $.extend(defaults, options);
 
         var cw = $(this).width();
 
-        if (opts.fullWidth=="on"){
-            opts.gap = Math.round(cw*0.08); //10%
-            opts.imageWidth=Math.round(cw*0.55);
+        if (opts.fullWidth == "on") {
+            opts.gap = Math.round(cw * 0.08); //10%
+            opts.imageWidth = Math.round(cw * 0.55);
         }
 
-        var ml = (cw-opts.imageWidth)/2
+        var ml = (cw - opts.imageWidth) / 2
 
         $(this).find("ul.js li").css({
-            marginRight:opts.gap
+            marginRight: opts.gap
         });
 
         $(this).find("ul.js").css({
-            marginLeft:ml
+            // marginLeft: ml
+            'transform': 'translate3D(' + ml + 'px,0,0)',
+            '-webkit-transform': 'translate3D(' + ml + 'px,0,0)',
+            '-moz-transform': 'translate3D(' + ml + 'px,0,0)'
         });
 
-        $(this).find("li>img").each(function (i) {
+        $(this).find("li>img").each(function(i) {
             $(this).data("index", i);
             $(this).css({
-                width:opts.imageWidth,
-                height:"auto"
+                width: opts.imageWidth,
+                height: "auto"
             });
         });
 
-        $(this).find("li>img").on("click",function () {
+        $(this).find("li>img").on("click", function() {
             var index = $(this).data("index");
-            var newlm = ml - index * (opts.imageWidth+opts.gap) - index*3; //new left margin value
+            var newlm = ml - index * (opts.imageWidth + opts.gap) - index * 3; //new left margin value
 
             $(container).find("ul.js").css({
-                marginLeft: (newlm)
+                // marginLeft: (newlm)
+                'transform': 'translate3D(' + newlm + 'px,0,0)',
+                '-webkit-transform': 'translate3D(' + newlm + 'px,0,0)',
+                'moz-transform': 'translate3D(' + newlm + 'px,0,0)'
             });
 
             $(container).find("li>img").css({
